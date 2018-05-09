b78a600e83a310de90d481d9f53052ffeacb98d0
ZGCTroy <zgc_troy@163.com>
2018-05-09 17:46:12 +0800

    8

--- 7、强化学习/Policy Network/main.py
+++ 7、强化学习/Policy Network/main.py
@@ -7,21 +7,21 @@ env = gym.make('CartPole-v0')
 env.reset()
 random_episodes = 0
 reward_sum = 0
-
-while random_episodes < 1:
-    env.render()
-    observation, reward, done, _ = env.step(np.random.randint(0, 2))
-    reward_sum += reward
-    if done:
-        random_episodes += 1
-        print("Reward for this episode was:", reward_sum)
-        reward_sum = 0
-        env.reset()
+env.render()
+# while random_episodes < 20:
+#     env.render()
+#     observation, reward, done, _ = env.step(np.random.randint(0, 2))
+#     reward_sum += reward
+#     if done:
+#         random_episodes += 1
+#         print("Reward for this episode was:", reward_sum)
+#         reward_sum = 0
+#         env.reset()
 
 # Hyper Parameters
 H = 50
 batch_size = 25
-learning_rate = 1e-3
+learning_rate = 1e-1
 D = 4
 gamma = 0.99
 
